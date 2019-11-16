b071738224212067977eb7fa3e41d021e86028c5
Marek <marek@mbp-marek.lan>
2019-11-16 18:04:51 +0100

    Initial commit

---
+++ src/main/java/pl/sda/VaadinToDo/VaadinToDoApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.VaadinToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinToDoApplication.class, args);
+	}
+
+}
